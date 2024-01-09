8270c5a273fb7457e70c36814e231d749b5d4215
andrewlalis <andrewlalisofficial@gmail.com>
2024-01-08 22:43:41 -0500

    Added basic side-panel help info, soon will add router implementation for pages.

--- src/main/java/com/andrewlalis/perfin/control/MainViewController.java
+++ src/main/java/com/andrewlalis/perfin/control/MainViewController.java
@@ -4,15 +4,21 @@ import com.andrewlalis.javafx_scene_router.AnchorPaneRouterView;
 import com.andrewlalis.perfin.view.BindingUtil;
 import com.andrewlalis.perfin.view.ProfilesStage;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import static com.andrewlalis.perfin.PerfinApp.router;
 
 public class MainViewController {
     @FXML public BorderPane mainContainer;
     @FXML public HBox breadcrumbHBox;
+
+    @FXML public Button showManualButton;
+    @FXML public Button hideManualButton;
+    @FXML public VBox manualVBox;
 
     @FXML public void initialize() {
         AnchorPaneRouterView routerView = (AnchorPaneRouterView) router.getView();
@@ -30,6 +36,13 @@ public class MainViewController {
                     return label;
                 }
         );
+
+        manualVBox.managedProperty().bind(manualVBox.visibleProperty());
+        manualVBox.setVisible(false);
+        showManualButton.managedProperty().bind(showManualButton.visibleProperty());
+        showManualButton.visibleProperty().bind(manualVBox.visibleProperty().not());
+        hideManualButton.managedProperty().bind(hideManualButton.visibleProperty());
+        hideManualButton.visibleProperty().bind(manualVBox.visibleProperty());
 
         router.navigate("accounts");
     }
@@ -54,5 +67,13 @@ public class MainViewController {
 
     @FXML public void viewProfiles() {
         ProfilesStage.open(mainContainer.getScene().getWindow());
+    }
+
+    @FXML public void showManual() {
+        manualVBox.setVisible(true);
+    }
+
+    @FXML public void hideManual() {
+        manualVBox.setVisible(false);
     }
 }
